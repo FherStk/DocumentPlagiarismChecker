e6b6a819238b9422e2daadd148c2a7038a77f7ef
Talessa <mjunoygomez@gmail.com>
2018-12-14 19:29:42 +0100

    3

--- src/API/API.cs
+++ src/API/API.cs
@@ -81,7 +81,7 @@ namespace DocumentPlagiarismChecker
         /// <param name="results">A set of file matching scores</param>
         public void WriteOutput(DisplayLevel level = DisplayLevel.COMPARATOR){
             //TODO: must be selected by settings
-            Outputs.Terminal t = new Outputs.Terminal();
+            Outputs.Terminal2 t = new Outputs.Terminal2();
             t.Write(this.MatchingResults, level);
         }
 
