654bdf37ddfbcb4a257efff39ffacab2554f9c99
salva <Sallop@sallop.com>
2021-05-01 19:30:19 +0200

    Traduccion S2P1

--- src/Comparators/ParagraphWordCounter/Comparator.cs
+++ src/Comparators/ParagraphWordCounter/Comparator.cs
@@ -35,10 +35,10 @@ namespace DocumentPlagiarismChecker.Comparators.ParagraphWordCounter
         /// </summary>
         /// <returns>The matching's results.</returns>
         public override ComparatorMatchingScore Run(){     
-            //This order is meant to improving performance
+            // Este pedido está destinado a mejorar el rendimiento.
             ExcludeSampleExactMatches(); 
-            ExcludeSamplePartialMatches(this.Left, 0.70f);  //TODO: threshold value must be get from settings; check if can be removed
-            ExcludeSamplePartialMatches(this.Right, 0.70f);  //TODO: threshold value must be get from settings; check if can be removed
+            ExcludeSamplePartialMatches(this.Left, 0.70f);  // TODO: el valor del umbral debe obtenerse de la configuración; comprobar si se puede quitar
+            ExcludeSamplePartialMatches(this.Right, 0.70f);  // TODO: el valor del umbral debe obtenerse de la configuración; comprobar si se puede quitar
             ExcludeExclussionListMatches();
             
             return ComputeMatching(CompareParagraphs(this.Left, this.Right));                                                        
