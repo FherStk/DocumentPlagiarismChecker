73b47cf889cd4ee98d61cc35449c17c66971e3c2
Alice Bartolozzi <git>
2021-05-08 01:04:14 +0200

    He modificado el metodo TerminalOutput y creado el txt del archivo terminalOutput.

--- src/Api.cs
+++ src/Api.cs
@@ -97,8 +97,8 @@ namespace DocumentPlagiarismChecker
         /// <param name="results">A set of file matching scores</param>
         public void WriteOutput(){
             //TODO: must be selected by settings
-            TerminalOutput t = new TerminalOutput(this.Settings);
-            t.Write(this.MatchingResults);
+            TerminalOutput terminal = new TerminalOutput(this.Settings);
+            terminal.Write(this.MatchingResults);
         }
 
         /// <summary>
