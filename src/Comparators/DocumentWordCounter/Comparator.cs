8c91682d6ad79a37e12bfcf4630109266ea8fe3b
Pol <pbermejo@elpuig.xeill.net>
2021-12-20 19:24:29 +0100

    commit pol2

--- src/Comparators/DocumentWordCounter/Comparator.cs
+++ src/Comparators/DocumentWordCounter/Comparator.cs
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2018 Fernando Porrino Serrano.
     This software it's under the terms of the GNU Affero General Public License version 3.
-    Please, refer to (https://github.com/FherStk/DocumentPlagiarismChecker/blob/master/LICENSE) for further licensing details.CommitPol
+    Please, refer to (https://github.com/FherStk/DocumentPlagiarismChecker/blob/master/LICENSE) for further licensing details.CommitPolM05---***+++
  */
  
 using System;
